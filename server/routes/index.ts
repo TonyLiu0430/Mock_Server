4d98c399fdc2ada9a036ae778da7fc179e59dd5d
TonyLiu0430 <tony04302020@gmail.com>
2024-11-16 15:58:50 +0800

    steal entire header

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,9 +1,5 @@
 export default eventHandler((event) => {
-  const Cookie = event.headers.get('Cookie')
-  console.log(Cookie)
+  console.log(event.headers)
 
-  const cookie = event.headers.get('cookie')
-  console.log(cookie)
-  
   return `Hello`
 });
