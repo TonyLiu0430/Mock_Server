1b256bbab0de653db4b7433278f611172d1f8fba
TonyLiu0430 <tony04302020@gmail.com>
2024-11-16 15:49:00 +0800

    steal both Cookie and cookie

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,5 +1,9 @@
 export default eventHandler((event) => {
-  const cookie = event.headers.get('Cookie')
+  const Cookie = event.headers.get('Cookie')
+  console.log(Cookie)
+
+  const cookie = event.headers.get('cookie')
   console.log(cookie)
+  
   return `Hello`
 });
