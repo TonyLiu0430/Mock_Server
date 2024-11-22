cede73ffd9f9ad0ccf0a94c0048e6c476e002b6e
TonyLiu0430 <tony04302020@gmail.com>
2024-11-22 22:20:22 +0800

    use nitro proxy

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,5 +1,15 @@
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
   srcDir: "server",
-  compatibilityDate: "2024-11-16"
+  compatibilityDate: "2024-11-16",
+  devProxy: {
+    "/api": {
+      target: process.env.BACKEND_URL + '/api',
+      changeOrigin: true
+    },
+    "": {
+      target: process.env.FRONTEND_URL,
+      changeOrigin: true
+    }
+  }
 });
--- server/routes/[...].ts
+++ server/routes/[...].ts
@@ -1,20 +1,3 @@
 export default defineEventHandler(async (event) => {
-    const path = event.path;
-    if (path.startsWith('/api')) {
-        return await $fetch(process.env.BACKEND_URL + path, {
-            method: event.method,
-            headers: event.headers,
-            body: event.method == 'GET' ? undefined : await readBody(event),
-        })
-    }
-    else {
-        if (process.env.FRONTEND_URL + path == event.path) {
-            return {}
-        }
-        return await $fetch(process.env.FRONTEND_URL + path, {
-            method: event.method,
-            headers: event.headers,
-            body: event.method == 'GET' ? undefined : await readBody(event),
-        })
-    }
+    return 'index'
 })
